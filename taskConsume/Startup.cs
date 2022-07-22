94dafbdaa199bb2eee318b44110ae63fe793a88e
fangyuanwan <fangyuanwan1989@gmail.com>
2022-07-22 16:14:53 +0800

    fix issue

--- taskConsume/Startup.cs
+++ taskConsume/Startup.cs
@@ -42,13 +42,13 @@ namespace taskConsume
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TaskApi v1"));
-
-            }
+            // if (env.IsDevelopment())
+            // {
+             //    app.UseDeveloperExceptionPage();
+              
+            // }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TaskApi v1"));
 
             app.UseHttpsRedirection();
 
